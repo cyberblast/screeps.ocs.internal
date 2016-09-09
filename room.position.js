eaa2850fe321f21d37bf976ec712eb82197d325b
MiKiOnEs <mikiones@gmail.com>
2016-09-09 10:47:44 +0100

    Find best position for mining container. More goodes in RoomPosition extension:   1. find open positions in range(range <= ) and at range (range == )   2. find subset of positions next to given position.

--- room.position.js
+++ room.position.js
@@ -54,8 +54,22 @@ let mod = {
             return this.road.length > 0;
         }
 
+        RoomPosition.prototype.getPositionsInRange = function (range) {
+            let x1 = Math.max(this.x - range, 0);
+            let y1 = Math.max(this.y - range, 0);
+            let x2 = Math.min(this.x + range, 49);
+            let y2 = Math.min(this.y + range, 49);
+            let ret = [];
+            for (var xi = x1; xi <= x2; xi++)
+                for (var yi = y1; yi <= y2; yi ++){
+                    let t = new RoomPosition(xi,yi,this.roomName);
+                    if (t.getRangeTo(this) <= range)
+                        ret.push(t);
+                }
+            return ret;
+        }
 
-        RoomPosition.prototype.getPositionsInRange = function ( range ) {
+        RoomPosition.prototype.getPositionsAtRange = function ( range ) {
             let x1 = Math.max(this.x - range, 0);
             let y1 = Math.max(this.y - range, 0);
             let x2 = Math.min(this.x + range, 49);
@@ -70,14 +84,28 @@ let mod = {
             return ret;
         };
 
-        RoomPosition.prototype.getOpenPositonsInRange = function (range) {
-            return this.getPositionsInRange(range).filter( p => p.isOpen());
+        RoomPosition.prototype.getOpenPositionsInRange = function (range) {
+            return this.getPositionsInRange(range).filter(p => p.isOpen());
+        }
+
+        RoomPosition.prototype.getOpenPositonsAtRange = function (range) {
+            return this.getPositionsAtRange(range).filter( p => p.isOpen());
         };
 
         // get closesestt point in range to second point .
         // f.e to build a conteiner 2 blocks away from controller and in closes position to spawn.
-        RoomPosition.prototype.getOpenPosiotionInRangeClosestTo = function( range, topos ) {
-            return topos.findClosestByPath(this.getOpenPositonsInRange(range));
+        RoomPosition.prototype.getOpenPosiotionAtRangeClosestTo = function( range, topos ) {
+            return topos.findClosestByPath(this.getOpenPositonsAtRange());
+        };
+
+        RoomPosition.prototype.isNextTo = function (position) {
+                const R = [1,0,-1];
+                return R.includes( this.x - position.x) && R.includes(this.y - position.y);
+        };
+
+        // get subset of positions next to this position from array
+        RoomPosition.prototype.getNextToFrom = function( pos_array) {
+            return pos_array.filter(e => e.isNextTo(this));
         };
 
     },
