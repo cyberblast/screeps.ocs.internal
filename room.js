e7c93fa9e35c65a2cd8e488ee321990c2bdf29dd
cyberblast <git@cyberblast.org>
2016-12-09 08:52:28 +0100

    fix for removed storage

--- room.js
+++ room.js
@@ -31,7 +31,7 @@ var mod = {
                         if( _.isUndefined(this._controller) ){
                             if( this.room.controller.memory.storage ){
                                 this._controller = [Game.getObjectById(this.room.controller.memory.storage)];
-                                if( !this._controller ) delete this.room.controller.memory.storage;
+                                if( !this._controller[0] ) delete this.room.controller.memory.storage;
                             } else {
                                 let byType = c => c.controller == true;
                                 this._controller = _.filter(this.all, byType);
