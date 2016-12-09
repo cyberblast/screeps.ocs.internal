00e989eddf77986b4b87fa1988d52dbac4c207bf
cyberblast <git@cyberblast.org>
2016-12-09 18:42:05 +0100

    dont evaluate controller-storage in foreign room

--- room.js
+++ room.js
@@ -29,7 +29,7 @@ var mod = {
                     configurable: true,
                     get: function() {
                         if( _.isUndefined(this._controller) ){
-                            if( this.room.controller.memory.storage ){
+                            if( this.room.my && this.room.controller.memory.storage ){
                                 this._controller = [Game.getObjectById(this.room.controller.memory.storage)];
                                 if( !this._controller[0] ) delete this.room.controller.memory.storage;
                             } else {
