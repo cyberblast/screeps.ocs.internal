4e6b24c4f19f7ab5d533d8a906f93049a7073e66
cyberblast <git@cyberblast.org>
2016-12-14 14:06:06 +0100

    fixing wrong require of module statistics

--- main.js
+++ main.js
@@ -130,6 +130,6 @@ module.exports.loop = function () {
     Spawn.loop();
 
     if( Memory.statistics && Memory.statistics.tick && Memory.statistics.tick + TIME_REPORT <= Game.time )
-        require(Memory.modules.statistics.path).loop();
+        load("statistics").loop();
     processReports();
 };
