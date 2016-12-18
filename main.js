fdbcf3dddb27fef2f0c15697a99618025ef70bf5
cyberblast <git@cyberblast.org>
2016-12-18 08:43:45 +0100

    viral Overrides

--- main.js
+++ main.js
@@ -4,16 +4,26 @@ module.exports.loop = function () {
     global.getPath = (modName, reevaluate = false) => {
         if (Memory.modules === undefined) 
             Memory.modules = {};
+        if (Memory.modules.viral === undefined) 
+            Memory.modules.viral = {};
         if( reevaluate || !Memory.modules[modName] ){
             let path = './custom.' + modName;
             try {
-                var a = require(path);
+                let a = require(path);
             }
             catch (e) {
                 path = './' + modName
             }
             finally {
                 Memory.modules[modName] = path;
+            }
+            path = './viral.' + modName;
+            try {
+                let viralOverride = require(path);
+                if( viralOverride ) Memory.modules.viral[modName] = true;
+            }
+            catch (e) {
+                delete Memory.modules.viral[modName];
             }
         }
         return Memory.modules[modName];
@@ -41,6 +51,22 @@ module.exports.loop = function () {
             } else if(mod == null) {
                 console.log(`Error loading module "${modName}"!<br/>${e.toString()}`);
             }
+        }
+        if( mod && Memory.modules.viral[modName] ) {
+            let viralOverride;
+            try {
+                viralOverride = require('./viral.' + modName);
+            }
+            catch (e) {
+                viralOverride = null;
+                if( e.message && e.message.indexOf('Unknown module') > -1 ){
+                    console.log(`Viral override for "${modName}" not found!`);
+                } else if(mod == null) {
+                    console.log(`Error loading viral override "${modName}"!<br/>${e.toString()}`);
+                }
+            }
+            if( viralOverride ) _.assign(mod, viralOverride);
+            else delete Memory.modules.viral[modName];
         }
         return mod;
     };
