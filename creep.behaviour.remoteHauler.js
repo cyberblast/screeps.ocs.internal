bb17e8a521e5c75de19934a6460b26ca09f1ad4d
Chris Panda <chrispandabear41@gmail.com>
2016-12-22 10:47:02 -0800

    Storing is already priority 1. Not needed

--- creep.behaviour.remoteHauler.js
+++ creep.behaviour.remoteHauler.js
@@ -46,12 +46,6 @@ module.exports = {
                 Creep.action.storing,
                 Creep.action.idle];
 
-            if ( creep.sum > creep.carry.energy ||
-                ( !creep.room.situation.invasion
-                && SPAWN_DEFENSE_ON_ATTACK
-                && creep.room.conserveForDefense && creep.room.relativeEnergyAvailable > 0.8)) {
-                    priority.unshift(Creep.action.storing);
-            }
             if (creep.room.structures.urgentRepairable.length > 0 ) {
                 priority.unshift(Creep.action.fueling);
             }
