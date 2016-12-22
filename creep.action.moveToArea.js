89fe0512aa265cbcaba113da266d3e0c852a0bdf
Chris Panda <chrispandabear41@gmail.com>
2016-12-22 13:31:50 -0800

    Fixing moveToArea

--- creep.action.moveToArea.js
+++ creep.action.moveToArea.js
@@ -1,9 +1,10 @@
 var action = new Creep.Action('moveToArea');
 action.targetRange = 3;
+action.renewTarget = false;
 action.isValidAction = creep => (typeof action.target[creep.data.creepType] === 'function');
-action.isAddableAction = creep => (action.target[creep.data.creepType] != null);
+action.isAddableAction = creep => (action.target[creep.data.creepType](creep) != null);
 action.isAddableTarget = function(){ return true; };
-action.newTarget = creep => action.target[creep.data.creepType];
+action.newTarget = creep => action.target[creep.data.creepType](creep);
 action.step = function(creep){
     if(CHATTY) creep.say(this.name, SAY_PUBLIC);
     let range = creep.pos.getRangeTo(creep.target);
@@ -34,8 +35,13 @@ action.target = {
                 }
             };
             _.forEach(containers, fullest);
-        }
 
+            // if the creep is already near the target don't return target.
+            let range = creep.pos.getRangeTo(target);
+            if( range <= 3) {
+                return null;
+            }
+        }
         return target;
     }
 },
