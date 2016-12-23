cc524d571ca44cef3b9352cf805e2101a454543e
cyberblast <git@cyberblast.org>
2016-12-23 15:49:49 +0100

    added hooper flag colors to internal

--- internalViral.global.js
+++ internalViral.global.js
@@ -1,9 +1,21 @@
 var mod = {
     extend: function(){
+        // warrior
         Creep.setup.warrior = load("creep.setup.warrior");
         Creep.behaviour.warrior = load("creep.behaviour.warrior");
+        // hopper
         Creep.setup.hopper = load("creep.setup.hopper");
-        Creep.behaviour.hopper = load("creep.behaviour.hopper");
+        Creep.behaviour.hopper = load("creep.behaviour.hopper");        
+        global.FLAG_COLOR.hopper = { // the room where tower is
+            color: COLOR_RED,
+            secondaryColor: COLOR_PURPLE,
+            filter: {'color': COLOR_RED, 'secondaryColor': COLOR_PURPLE }
+        };
+        global.FLAG_COLOR.hopperHome = { // room to heal in
+            color: COLOR_RED,
+            secondaryColor: COLOR_BLUE,
+            filter: {'color': COLOR_RED, 'secondaryColor': COLOR_BLUE }
+        };
     }
 }
 module.exports = mod;
