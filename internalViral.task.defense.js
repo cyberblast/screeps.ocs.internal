704aa616a2c1319c4996734c078d5418a92aaa91
cyberblast <git@cyberblast.org>
2017-01-16 20:40:28 +0100

    unifying task creep definitions (#169)

--- internalViral.task.defense.js
+++ internalViral.task.defense.js
@@ -1,7 +1,12 @@
 var mod = {
-    defenderFixedBody: [ATTACK, MOVE],
-    defenderMultiBody: [TOUGH, ATTACK, RANGED_ATTACK, HEAL, MOVE, MOVE],
-    defenderName: 'defender',
+    creep: {
+        defender: {
+            fixedBody: [ATTACK, MOVE],
+            multiBody: [TOUGH, ATTACK, RANGED_ATTACK, HEAL, MOVE, MOVE],
+            name: "defender", 
+            behaviour: "warrior"
+        },
+    },
 };
 
 module.exports = mod; 
--- internalViral.task.guard.js
+++ internalViral.task.guard.js
@@ -1,29 +1,11 @@
 var mod = {
-    checkForRequiredCreeps: (flag) => {
-        let memory = Task.guard.memory(flag);
-        // count creeps
-        let count = memory.queued.length + memory.spawning.length + memory.running.length;
-        // if creeps below requirement
-        if( count < 1 ) {
-            // add creep
-            let room = Room.bestSpawnRoomFor(flag.pos.roomName);
-            let fixedBody = [ATTACK, MOVE];
-            let multiBody = [TOUGH, ATTACK, RANGED_ATTACK, HEAL, MOVE, MOVE];
-            let name = 'warrior-' + flag.name;
-
-            let creep = {
-                parts: Creep.Setup.compileBody(room, fixedBody, multiBody, true),
-                name: name,
-                setup: 'warrior',
-                destiny: { task: "guard", flagName: flag.name }
-            };
-
-            room.spawnQueueMedium.push(creep);
-            memory.queued.push({
-                room: room.name,
-                name: name
-            });
-        }
+    creep: {
+        guard: {
+            fixedBody: [ATTACK, MOVE],
+            multiBody: [TOUGH, ATTACK, RANGED_ATTACK, HEAL, MOVE, MOVE],
+            name: "guard", 
+            behaviour: "warrior"
+        },
     }
 };
 
