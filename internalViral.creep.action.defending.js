5c7632aa09dc0a063a5321309c70329a2e893b1c
Chobobobo <chobobobo@users.noreply.github.com>
2017-02-26 15:45:27 +0800

    Reduce defense creep CPU usage and clean up code so it reads more easily.

--- internalViral.creep.action.defending.js
+++ internalViral.creep.action.defending.js
@@ -19,10 +19,7 @@ mod.run = {
                 //     if(direction) creep.move(direction);
                 // }
             }
-            if( range < 3 ){
-                let direction = creep.target.pos.getDirectionTo(creep);
-                if(direction) creep.move(direction);
-            }
+            if( range < 3 ) creep.fleeMove();
         }
 
         // attack ranged
@@ -30,42 +27,35 @@ mod.run = {
         if(targets.length > 2) { // TODO: precalc damage dealt
             if(CHATTY) creep.say('MassAttack');
             creep.attackingRanged = creep.rangedMassAttack() == OK;
-            return;
-        }
-        if( range < 4 ) {
+        } else if( range < 4 ) {
             creep.attackingRanged = creep.rangedAttack(creep.target) == OK;
-            return;
-        }
-        if(targets.length > 0){
+        } else if(targets.length > 0){
             creep.attackingRanged = creep.rangedAttack(targets[0]) == OK;
         }
     },
     sourceKiller: function(creep) {
-        if( !creep.flee ){
+        if( !creep.flee && creep.pos.getRangeTo(creep.target) > 1 ){
             creep.travelTo(creep.target);
         }
         // attack
         let keeperLair = [];
         let keeperFind = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {return (structure.structureType == STRUCTURE_KEEPER_LAIR)}
+                filter: (structure) => structure.structureType == STRUCTURE_KEEPER_LAIR
         });
-        for(index in keeperFind){
+        for(let index in keeperFind){
             keeperLair.push(keeperFind[index]);
         }
-        let lowLair = keeperFind.sort(function(a, b){return a.ticksToSpawn - b.ticksToSpawn});
+        let lowLair = keeperFind.sort( (a, b) => a.ticksToSpawn - b.ticksToSpawn);
         creep.memory.lairs = lowLair[0];
-    
-        //console.log(JSON.stringify(test[0]));
-        //if(creep.target.length > 0){
+
         let attacking = creep.attack(creep.target);
         if( attacking == ERR_NOT_IN_RANGE ) {
             let targets = creep.pos.findInRange(creep.room.hostiles, 1);
-            if( targets.length > 0)
-                creep.attacking = creep.attack(targets[0]) == OK;
+            if( targets.length > 0) creep.attacking = creep.attack(targets[0]) == OK;
         } else creep.attacking = attacking == OK;  
     },
     melee: function(creep) {
-        if( !creep.flee ){
+        if( !creep.flee && creep.pos.getRangeTo(creep.target) > 1 ){
             creep.travelTo( creep.target );
             // FIXME: need to set a custom costMatrix to respect ramparts.
             // let path = creep.room.findPath(creep.pos, creep.target.pos);
@@ -92,7 +82,7 @@ mod.run = {
         let hasRangedAttack = creep.hasActiveBodyparts(RANGED_ATTACK);
         if( !creep.flee ){
             if( hasAttack ){
-                creep.travelTo( creep.target );
+                if( range > 1 ) creep.travelTo( creep.target );
                 // FIXME: need to set a custom costMatrix to respect ramparts.
                 // let path = creep.room.findPath(creep.pos, creep.target.pos, {'maxRooms': 1});
                 // // not standing in rampart or next step is rampart as well
@@ -104,8 +94,7 @@ mod.run = {
                 //     creep.move(path[0].direction);
                 // }
             } else if( hasRangedAttack ) {
-                if( range > 3 ){
-                    creep.travelTo( creep.target );
+                if( range > 3 ) creep.travelTo( creep.target );
                     // FIXME: need to set a custom costMatrix to respect ramparts.
                     // let path = creep.room.findPath(creep.pos, creep.target.pos, {'maxRooms': 1});
                     // if( path && path.length > 0 ) {
@@ -118,12 +107,7 @@ mod.run = {
                     //     let direction = creep.pos.getDirectionTo(creep.target);
                     //     if( direction ) creep.move(direction);
                     // }
-                }
-                if( range < 3 ){
-                    //var direction = creep.target.pos.getDirectionTo(creep);
-                    //creep.move(direction);
-                    creep.fleeMove();
-                }
+                if( range < 3 ) creep.fleeMove();
             } else creep.flee = true;
         }
         // attack
@@ -131,8 +115,7 @@ mod.run = {
             let attacking = creep.attack(creep.target);
             if( attacking == ERR_NOT_IN_RANGE ) {
                 let targets = creep.pos.findInRange(creep.room.hostiles, 1);
-                if( targets.length > 0)
-                    creep.attacking = creep.attack(targets[0]) == OK;
+                if( targets.length > 0) creep.attacking = creep.attack(targets[0]) == OK;
             } else creep.attacking = attacking == OK;
         }
         // attack ranged
@@ -141,13 +124,9 @@ mod.run = {
             if(targets.length > 2) { // TODO: precalc damage dealt
                 if(CHATTY) creep.say('MassAttack');
                 creep.attackingRanged = creep.rangedMassAttack() == OK;
-                return;
-            }
-            if( range < 4 ) {
+            } else if( range < 4 ) {
                 creep.attackingRanged = creep.rangedAttack(creep.target) == OK;
-                return;
-            }
-            if(targets.length > 0){
+            } else if(targets.length > 0){
                 creep.attackingRanged = creep.rangedAttack(targets[0]) == OK;
             }
         }
