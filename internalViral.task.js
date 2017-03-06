bfc90c6815577da949edf162763c11554eddd7e9
Dan (Asku) <askewholdings@gmail.com>
2017-03-06 17:01:34 +0000

    flush and register

--- internalViral.task.js
+++ internalViral.task.js
@@ -1,13 +1,45 @@
 let mod = {};
 module.exports = mod;
-mod.register = function () {
-    let tasks = [
-
+mod.flush = function () {
+    const tasks = [
+        Task.guard,
+        Task.defense,
+        Task.claim,
+        Task.reserve,
+        Task.mining,
+        Task.pioneer,
+        Task.attackController,
+        Task.robbing,
+        Task.reputation,
+        Task.powerMining,
+        Task.train,
     ];
-    var loop = task => {
-        task.register();
+    for (let i = tasks.length - 1; i >= 0; i--) {
+        if (tasks[i].flush) {
+            tasks[i].flush();
+        }
     }
-    _.forEach(tasks, loop);
+};
+// register tasks (hook up into events)
+mod.register = function () {
+    const tasks = [
+        Task.guard,
+        Task.defense,
+        Task.claim,
+        Task.reserve,
+        Task.mining,
+        Task.pioneer,
+        Task.attackController,
+        Task.robbing,
+        Task.reputation,
+        Task.powerMining,
+        Task.train,
+    ];
+    for (let i = tasks.length - 1; i >= 0; i--) {
+        if (tasks[i].register) {
+            tasks[i].register();
+        }
+    }
 };
 
 
