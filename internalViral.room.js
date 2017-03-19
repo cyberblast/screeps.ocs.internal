e0710890c5f09bf58cf775573c616237304a4c26
Dan (Asku) <askewholdings@gmail.com>
2017-03-19 15:48:52 +0000

    room stuff (commented)

--- internalViral.room.js
+++ internalViral.room.js
@@ -25,5 +25,27 @@ var mod = {
             },
         });
     }
+    /* No idea how to do this bit
+    Room.prototype.checkPowerBank = function() {
+    if (!this.powerBank) return; // no power bank in room
+    	//power > 2500, and ticksToDecay > 4500
+        const currentFlags = FlagDir.count(FLAG_COLOR.invade.powerMining, this.powerBank.pos, false);
+    	const flagged = FlagDir.find(FLAG_COLOR.invade.powerMining, this.powerBank.pos, true);
+    	if(!flagged && currentFlags < MAX_AUTO_POWER_MINING_FLAGS){
+    	    if(this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
+    		    // Place a flag
+    		    this.createFlag(this.powerBank.pos, null, FLAG_COLOR.invade.powerMining.color, FLAG_COLOR.invade.powerMining.secondaryColor);
+    	    }
+        }
+    };
+    
+    mod.analyze extend
+    
+    if(AUTO_POWER_MINING) room.checkPowerBank();
+    
+    */
+    
+    
+    
 };
 module.exports = mod;
