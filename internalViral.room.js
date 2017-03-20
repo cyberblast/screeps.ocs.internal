3bc3912ddeffe83f98173bcd8fcf8c68dd97fc10
Dan (Asku) <askewholdings@gmail.com>
2017-03-20 23:37:55 +0000

    global flag

--- internalViral.room.js
+++ internalViral.room.js
@@ -29,12 +29,12 @@ var mod = {
     Room.prototype.checkPowerBank = function() {
     if (!this.powerBank) return; // no power bank in room
     	//power > 2500, and ticksToDecay > 4500
-        const currentFlags = FlagDir.count(FLAG_COLOR.invade.powerMining, this.powerBank.pos, false);
-    	const flagged = FlagDir.find(FLAG_COLOR.invade.powerMining, this.powerBank.pos, true);
+        const currentFlags = FlagDir.count(FLAG_COLOR.powerMining, this.powerBank.pos, false);
+    	const flagged = FlagDir.find(FLAG_COLOR.powerMining, this.powerBank.pos, true);
     	if(!flagged && currentFlags < MAX_AUTO_POWER_MINING_FLAGS){
     	    if(this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
     		    // Place a flag
-    		    this.createFlag(this.powerBank.pos, null, FLAG_COLOR.invade.powerMining.color, FLAG_COLOR.invade.powerMining.secondaryColor);
+    		    this.createFlag(this.powerBank.pos, null, FLAG_COLOR.powerMining.color, FLAG_COLOR.powerMining.secondaryColor);
     	    }
         }
     };
