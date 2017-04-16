bec504635542bac29caefb42548fa165167ef867
Spedwards <Spedwards@users.noreply.github.com>
2017-04-17 00:51:54 +1000

    Change/diplomatic dictatorship (#313)
    * Diplomatic Power
    
    * Clean up
    
    * Load actions
    
    * Don't add flag to cloaked power bank.
    
    * Stuff
    
    * Traveler is confusing
    
    * Return length
    
    * Until we set to accessible positions
    
    * Flipping the script
    
    * Return
    
    * hits -> hasActiveBodyparts
    
    * get the miner to register so the healers can find it, refactored powerHealer so they default to idle properly, and don’t allow powerMining related creeps to path through hostile rooms, that’s a bad idea.
    
    * stop healers from constantly setting travel on the miner once they are adjacent.
    
    * Correctly remove flag
    
    Would never get to action.work if powerBank doesn't exist so flag would never get removed.
    
    * Cloak flag as precaution
    
    * Today's commit is brought you by the letter K
    
    Because it was Karl's fault.
    
    * extricate logic and null check.
    
    * powerMiners recycle.
    
    * harvestPower doesn’t need to care about healers, let the task worry about that, also healers can use ranger.heal for consistency
    
    * typo
    
    * update powerHealer so that it stays around to heal the poor miner before recycling.
    
    * stop powerMiners from killing themselves.
    
    * powerHealers now try to stick to 2 per powerMiner
    
    * Only count loss if what opposition says is a valid play
    
    * Invalidate Action
    
    * Invalidate Mining Action
    
    * fix undefined miner
    
    * Remove unnecessary
    
    * Trust the system.
    
    * Avoid conflicts
    
    * correct bad merge
    
    * correct typo
    
    * give power mining flags names based on their room so we don’t have issues with haulers heading to the wrong room
    
    * and get the colour right.
    
    * shorter creep names are better

--- internalViral.parameter.js
+++ internalViral.parameter.js
@@ -4,6 +4,9 @@ let mod = {
 	CONTROLLER_SIGN_MESSAGE: `Territory of ${_ME}, an Open Collaboration Society member! (https://github.com/ScreepsOCS)`,
         AUTO_POWER_MINING: true, //set to false to disable power mining (recomended until 1-2 RCL8+ rooms)
         MAX_AUTO_POWER_MINING_FLAGS: 2,
+    ACTION_SAY: {
+        HARVESTPOWER: String.fromCodePoint(0x26CF),
+    },
         POWER_MINE_LOG: true, //displays power mining info in console
 };
 module.exports = mod;
