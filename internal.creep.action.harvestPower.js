5651ff26a5e99ad0087dd89d46456191644439b7
Spedwards <improbablepuzzle@gmail.com>
2017-04-22 15:47:57 +1000

    Action.HarvestPower

--- internal.creep.action.harvestPower.js
+++ internal.creep.action.harvestPower.js
@@ -1,39 +1,45 @@
-const action = new Creep.Action('harvestPower');
-module.exports = action;
-action.renewTarget = false;
-
-action.isValidAction = function(creep) {
-    const powerBank = creep.room.powerBank && !creep.room.powerBank.cloak;
-    if (!powerBank) {
-        const flag = this.targetFlag(creep);
-        if (flag) {
-            flag.cloaking = Infinity;
-            flag.remove();
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.renewTarget = false;
+    }
+    
+    isValidAction(creep) {
+        const powerBank = creep.room.powerBank && !creep.room.powerBank.cloak;
+        if (!powerBank) {
+            const flag =  this.targetFlag(creep);
+            if (flag) {
+                flag.cloaking = Infinity;
+                flag.remove();
+            }
+            return false;
         }
-        return false;
+        if (creep.data.diplomacyGame && _.some(creep.room.creeps, c => c.owner.username !== creep.data.diplomacyGame && Creep.action.diplomacy.isValidTarget(c))) return false;
+        return creep.hasBodyparts(ATTACK);
     }
-    if (creep.data.diplomacyGame && _.some(creep.room.creeps, c => c.owner.username !== creep.data.diplomacyGame && Creep.action.diplomacy.isValidTarget(c))) return false;
-    return creep.hasBodyparts(ATTACK);
-};
-
-action.isValidTarget = function(target) {
-    return target && target.power && target.power > 0 && !target.cloak;
-};
-
-action.newTarget = function(creep) {
-    const flag = this.targetFlag(creep);
-    if (!flag) return;
-    return _.find(flag.pos.lookFor(LOOK_STRUCTURES), s => s instanceof StructurePowerBank);
-};
-
-action.targetFlag = function(creep) {
-    return (creep.data.destiny && Game.flags[creep.data.destiny.targetName || creep.data.destiny.flagName || creep.data.flagName])
-        || FlagDir.find(FLAG_COLOR.powerMining, creep.pos);
-};
-
-action.work = function(creep) {
-    Population.registerCreepFlag(creep, action.targetFlag(creep));
-    if (creep.hits > 100) {
-        creep.attack(creep.target);
+    
+    isValidTarget(target) {
+        return super.isValidTarget(target) && target.power && target.power > 0 && !target.cloak;
     }
+    
+    newTarget(creep) {
+        const flag = this.targetFlag(creep);
+        if (!flag) return;
+        return _.find(flag.pos.lookFor(LOOK_STRUCTURES), s => s instanceof StructurePowerBank);
+    }
+    
+    targetFlag(creep) {
+        return (creep.data.destiny && Game.flags[creep.data.destiny.targetName]) || FlagDir.find(FLAG_COLOR.powerMining, creep.pos);
+    }
+    
+    work(creep) {
+        Population.registerCreepFlag(creep, this.targetFlag(creep));
+        if (creep.hits > 100) {
+            creep.attack(creep.target);
+        }
+    }
+    
 };
+module.exports = new action('harvestPower');
