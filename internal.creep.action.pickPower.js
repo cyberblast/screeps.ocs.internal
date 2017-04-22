80bc3271863600f52a93417d1799c9b8c45845dc
Spedwards <improbablepuzzle@gmail.com>
2017-04-22 16:06:03 +1000

    Action.PickPower

--- internal.creep.action.pickPower.js
+++ internal.creep.action.pickPower.js
@@ -1,67 +1,74 @@
-let action = new Creep.Action('pickPower');
-module.exports = action;
-action.maxPerAction = 100;
-action.maxPerTarget = 5;
-action.isValidAction = function(creep){
-    return ( creep.sum < creep.carryCapacity );
-};
-action.isValidTarget = function(target){
-    return (target != null && target.amount != null && target.amount > 0);
-};
-action.isAddableAction = function(creep){
-    if( creep.data.creepType.indexOf('remote') > 0 ) return true;
-    else return (this.maxPerAction === Infinity || !creep.room.population || !creep.room.population.actionCount[this.name] || creep.room.population.actionCount[this.name] < this.maxPerAction);
-};
-action.isAddableTarget = function(target, creep){
-    let max;
-    if( creep.data.creepType.indexOf('remote') > 0 ) max = Infinity;
-    else max =  this.maxPerTarget;
-    let pickers = target.targetOf ? _.filter(target.targetOf, {actionName: 'pickPower'}) : [];
-    return (!target.targetOf || !pickers.length || ((pickers.length < max) && target.amount > _.sum( pickers.map( t => t.carryCapacityLeft))));
-};
-action.newTarget = function(creep){
-    let target;
-    if( creep.room.situation.invasion ) {
-        // pickup near sources only
-        target = creep.pos.findClosestByPath(creep.room.droppedResources, {
-            filter: (o) => this.isAddableTarget(o, creep) && o.pos.findInRange(creep.room.sources, 1).length > 0
-        });
-    } else {
-        if ( creep.room.storage && creep.room.storage.my ) {
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.maxPerAction = 100;
+        this.maxPerTarget = 5;
+    }
+    
+    isValidAction(creep) {
+        return creep.sum < creep.carryCapacity;
+    }
+    
+    isValidTarget(creep) {
+        return super.isValidTarget(creep) && !!target.amount;
+    }
+    
+    isAddableAction(creep) {
+        if (creep.data.creepType.includes('remote')) return true;
+        return super.isAddableAction(creep);
+    }
+    
+    isAddableTarget(target, creep) {
+        const max = creep.data.creepType.includes('remote') ? Infinity : this.maxPerTarget;
+        const pickers = target.targetOf ? _.filter(target.targetOf, {actionName: this.name}) : [];
+        return !target.targetOf || !pickers.length || (pickers.length < max && target.amount > _.sum(pickers.map(t => t.carryCapacityLeft)));
+    }
+    
+    newTarget(creep) {
+        let target;
+        if (creep.room.situation.invasion) {
+            // pickup near sources only
             target = creep.pos.findClosestByPath(creep.room.droppedResources, {
-                filter: (o) => ( o.resourceType != RESOURCE_POWER && this.isAddableTarget(o, creep))
-                });
-
+                filter: o => this.isAddableTarget(o, creep) && o.pos.findInRange(creep.room.sources, 1).length > 0
+            });
         } else {
             target = creep.pos.findClosestByPath(creep.room.droppedResources, {
-                filter: (o) => ( o.resourceType == RESOURCE_POWER && this.isAddableTarget(o, creep))
+                filter: o => {
+                    if (!this.isAddableTarget(o, creep)) return false;
+                    if (creep.room.storage && creep.room.storage.my) {
+                        return o.resourceType !== RESOURCE_POWER;
+                    }
+                    return o.resourceType === RESOURCE_POWER;
+                }
             });
         }
+        return target;
     }
-    return target;
-};
-action.work = function(creep){
-    var result = creep.pickup(creep.target);
-    if( result == OK ){
-        if( creep.sum < creep.carryCapacity*0.8 ) {
-            // is there another in range?
-            let loot = creep.pos.findInRange(creep.room.droppedResources, 1, {
-                filter: (o) => o.resourceType != RESOURCE_POWER && this.isAddableTarget(o, creep)
-            });
-            if( !loot || loot.length < 1 ) loot = creep.pos.findInRange(creep.room.droppedResources, 1, {
-                filter: (o) => this.isAddableTarget(o, creep)
-            });
-            if( loot && loot.length > 0 ) {
-                this.assign(creep, loot[0]);
-                return result;
+    
+    work(creep) {
+        let result = creep.pickup(creep.target);
+        if (result === OK) {
+            if (creep.sum < creep.carryCapacity * 0.8) {
+                // is there another in range?
+                let loot = creep.pos.findInRange(creep.pos.droppedResources, 1, {
+                    filter: o => o.resourceType !== RESOURCE_POWER && this.isAddableTarget(o, creep)
+                });
+                if (!loot || loot.length < 1) loot = creep.pos.findInRange(creep.room.droppedResources, 1, {
+                    filter: o => this.isAddableTarget(o, creep)
+                });
+                if (loot && loot.length) {
+                    this.assign(creep, loot[0]);
+                    return result;
+                }
             }
+            // unregister
+            delete creep.data.actionName;
+            delete creep.data.targetId;
         }
-        // unregister
-        delete creep.data.actionName;
-        delete creep.data.targetId;
+        return result;
     }
-    return result;
+    
 };
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(8681), SAY_PUBLIC);
-};
+module.exports = new action('pickPower');
